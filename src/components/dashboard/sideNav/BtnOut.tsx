44aa64fbc679a03610aba42ccb39d5e507fc8e00
Ale <alejandro79861@gmail.com>
2025-05-03 19:27:08 -0500

    update metadata for todo-web and refactor signout button component

--- src/components/dashboard/sideNav/BtnOut.tsx
+++ src/components/dashboard/sideNav/BtnOut.tsx
@@ -1,19 +1,15 @@
 import { signOut } from "@/auth";
 
 export default function BtnOut() {
+  const handleOut = async () => {
+    await signOut();
+  };
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut();
-      }}
+    <button
+      onClick={handleOut}
+      className="bg-danger rounded-full px-4 py-2 my-2 text-white cursor-pointer"
     >
-      <button
-        className="bg-danger rounded-full px-4 py-2 my-2 text-white cursor-pointer"
-        type="submit"
-      >
-        Signout
-      </button>
-    </form>
+      Signout
+    </button>
   );
 }
